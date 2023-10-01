98b73da1e59012e6a129b70b4afa4ef54f9aeb7e
PJPhukan <paragjyotiphukan2004@gmail.com>
2023-10-01 12:28:40 +0530

    Functions in  C++

---
+++ functions.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+/*
+int Power(int a,int b){
+    int ans=pow(a,b);
+    return ans;
+}
+
+*/
+
+/*
+
+bool isEven(int n){
+    if (n&1)
+    {
+        return 0;
+    }
+    return 1;
+
+
+}
+
+
+*/
+
+/*
+// Find nCr
+
+int Factiorial(int n){
+    int fact=1;
+    for (int i = 1; i <= n; i++)    {
+        fact=fact*1;
+    }
+    return fact;
+}
+
+int nCr(int n,int r){
+
+    // int num=Factiorial(n);
+    // int deno=Factiorial(r)*Factiorial(n-r);
+    // int nCr=num/deno;
+
+// OR
+
+    // int nCr=Factiorial(n)/(Factiorial(r)* Factiorial(n-r));
+
+    return nCr;
+
+
+}
+*/
+
+
+/*
+bool isPrime(int n){
+    for (int i = 2; i < n/2; i++)
+    {
+        if (n%i==0)
+        {
+            return 0;
+        }
+        
+    }
+    return 1;
+    
+}
+
+*/
+int main()
+{
+    /*
+    int a,b;
+    cout<<"Enter the number : ";
+    cin>>a>>b;
+    // cout<<a<<"  Power  "<<b<<"  : "<<Power(a,b);
+    */
+    // cout<<isEven(24);
+
+    /*
+        int n,r;
+        cout<<"Enter n and r : ";
+        cin>>n>>r;
+        cout<<"The value of "<<n<<"C"<<r<<"is :"<<nCr(n,r);
+
+        */
+
+       
+       /*
+       int n;
+       cout<<"Enter the number : ";
+       cin>>n;
+       if(isPrime(n)){
+            cout<<"Prime !";
+       }
+       else{
+        cout<<"Not Prime !";
+       }
+       */
+    return 0;
+}
