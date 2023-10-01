2bd978257505fa549147adfffaaa9fef6ad30df1
PJPhukan <paragjyotiphukan2004@gmail.com>
2023-10-01 10:03:31 +0530

    Switch Case statement

---
+++ switch.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /*
+    int number=1;
+    switch (number)
+    {
+    case 1:
+        cout<<"The number is one";
+        break;
+    
+    default:
+        break;
+    }
+
+    */
+
+   //CALCULATOR
+   /*
+   int num1,num2;
+   char ch;
+   cout<<"Enter First Number :";
+   cin>>num1;
+   cout<<"Enter Operation which you want (+,_,*,/,%)";
+   cin>>ch;
+   cout<<"Enter Second Number :";
+   cin>>num2;
+   switch (ch)
+   {
+   case '+':
+    cout<<"Number1 + Number2 = "<<(num1+num2)<<endl;
+    break;
+   case '-':
+    cout<<"Number1 - Number2 = "<<(num1-num2)<<endl;
+    break;
+   case '*':
+    cout<<"Number1 * Number2 = "<<(num1*num2)<<endl;
+    break;
+   case '/':
+    cout<<"Number1 / Number2 = "<<(num1/num2)<<endl;
+    break;
+   case '%':
+    cout<<"Number1 % Number2 = "<<(num1%num2)<<endl;
+    break;
+   
+   default:
+   cout<<"Invalid Operator!"<<endl;
+    break;
+   }
+   */
+
+  
+    return 0;
+}
