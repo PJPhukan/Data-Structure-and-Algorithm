5f6e6c28863c07d8d1a120bc62193f4045cb4da2
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-01-01 22:50:54 +0530

    Doubly linked list ( Insert head,tail,any possition and delete from any possitions).

--- Linked_list.cpp
+++ Linked_list.cpp
@@ -6,7 +6,7 @@
 #include <string>
 #include <vector>
 using namespace std;
-
+/*
 // SINGLY LINKED LIST
 // creating a node
 class Node
@@ -59,7 +59,7 @@ void Insert_tail(Node *&tail, int data)
     tail = temp;
 
     // Another way to insert at the end if user pass the head pointer
-    /*
+
     Node *head_temp = tail;
     while (head_temp->next != NULL)
     {
@@ -67,8 +67,8 @@ void Insert_tail(Node *&tail, int data)
     }
     head_temp->next = temp;
     head_temp = temp;
-    */
 }
+
 
 // Delete node from any possition
 void Delete_node(Node *&head, int pos)
@@ -103,9 +103,109 @@ void Display(Node *head)
     }
     cout << endl;
 }
+ */
+
+/*
+// DOUBLY LINKED LIST
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node() {}
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+// Insert node at the head
+void Doubly_insert_head(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    head->prev = temp;
+    temp->next = head;
+    head = temp;
+}
+
+// Insert at the tail
+void Doubly_insert_tail(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    Node *temp_head = head;
+    while (temp_head->next != NULL)
+    {
+        temp_head = temp_head->next;
+    }
+    temp->prev = temp_head;
+    temp_head->next = temp;
+}
+
+// Insert node at any possition
+void Doubly_insert_mid(Node *&head, int data, int pos)
+{
+    if (pos == 1)
+    {
+        Doubly_insert_head(head, data);
+        return;
+    }
+    Node *temp = new Node(data);
+    Node *temp_head = head;
+    int cnt = 2;
+    while (cnt < pos && temp_head != NULL)
+    {
+        temp_head = temp_head->next;
+        cnt++;
+    }
+    temp->next = temp_head->next;
+    temp_head->next->prev = temp;
+    temp->prev = temp_head;
+    temp_head->next = temp;
+}
+
+// Delete node from any possition
+void Doubly_delete(Node *&head, int pos)
+{
+    Node *temp = head;
+    if (pos == 1)
+    {
+        head = temp->next;
+        delete temp;
+        return;
+    }
+    int cnt = 2;
+    while (cnt < pos)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+    Node *current = temp->next;
+    temp->next = current->next;
+    current->next->prev = temp;
+    delete current;
+}
+
+// print doubly linked list all nodes
+void Print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+*/
 
 int main()
 {
+
+    /*
+//SINGLY LINKED LIST
+
     // creating the head pointer
     Node *head = NULL;
 
@@ -133,6 +233,30 @@ int main()
     Delete_node(head, 5);
     Delete_node(head, 1);
     Display(head);
+    */
+
+    /*
+     // DOUBLY LINKED LIST
+     Node *node1 = new Node(8);
+     Node *head = node1;
+     // Insert head
+     Doubly_insert_head(head, 13);
+     Doubly_insert_head(head, 3);
+
+     // Insert tail
+     Doubly_insert_tail(head, 123);
+     Doubly_insert_tail(head, 33);
+
+     // Insert mid
+     Doubly_insert_mid(head, 64, 1);
+     Doubly_insert_mid(head, 9, 3);
+     Print(head);
+
+     // Delete node from any possitions
+     Doubly_delete(head, 2);
+     Doubly_delete(head, 1);
+     Print(head);
+    */
 
     return 0;
 }
